4c90d3a31c684675b908a2b3af2ba978025e563d
annadrg <annaderuig@gmail.com>
2021-04-10 14:31:29 +0200

    Add tag actions and selectors

--- src/store/tag/reducer.ts
+++ src/store/tag/reducer.ts
@@ -1,11 +1,34 @@
 import { TagActions, TagState } from "./types";
 
 const initialState: TagState = {
-  tags: [],
+  userTags: [],
 };
 
 export default function reducer(state = initialState, action: TagActions) {
   switch (action.type) {
+    case "tag/tagsFetched":
+      return {
+        userTags: [...action.payload],
+      };
+    case "tag/addOne":
+      return {
+        userTags: [...state.userTags, action.payload],
+      };
+
+    case "tag/updateOne":
+      return {
+        userTags: state.userTags.map((tag) => {
+          if (tag.id !== action.payload.id) {
+            return tag;
+          }
+
+          return action.payload;
+        }),
+      };
+    case "tag/deleteOne":
+      return {
+        userTags: state.userTags.filter((tag) => tag.id !== action.payload),
+      };
     default:
       return state;
   }
--- src/store/tag/types.ts
+++ src/store/tag/types.ts
@@ -1,14 +1,24 @@
 // State type
 export type TagState = {
-  tags: Tag[];
+  userTags: Tag[];
 };
 
 // Action types
-export type TagActions = { type: string; payload: any }; // To change
+export type TagActions =
+  | { type: "tag/tagsFetched"; payload: Tag[] }
+  | { type: "tag/addOne"; payload: Tag }
+  | { type: "tag/updateOne"; payload: Tag }
+  | { type: "tag/deleteOne"; payload: number };
 
 // Tag type
 export type Tag = {
   id: number;
   name: string;
-  color: string;
+  color?: string;
+};
+
+// New tag type
+export type NewTag = {
+  name: string;
+  color?: string;
 };
